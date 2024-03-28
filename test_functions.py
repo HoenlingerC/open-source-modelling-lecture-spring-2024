074d25e1a380a60ddfaa59013b7efbf573b5ae0b
HoenlingerC <e1326676@student.tuwien.ac.at>
2024-03-28 20:28:06 +0100

    Update test_functions

--- test_functions.py
+++ test_functions.py
@@ -1,9 +1,17 @@
 """This is the test file"""
 
 from functions import add
+from functions import multiply
 
 
 def test_add():
     assert add(3, 5) == 8
     assert add(6, 1) == 7
     assert add(200, 50) == 250
+    
+
+def test_multiply():
+    assert multiply(3, 5) == 15
+    assert multiply(6, 1) == 6
+    assert multiply(200, 50) == 10000
+    assert multiply(0, 50) == 0
